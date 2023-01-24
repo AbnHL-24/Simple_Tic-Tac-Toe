4102e0eb56536bab607e2ba3bae3a239aded2b81
AbnHL-24 <abneralberto-Hernandezlopez@cunoc.edu.gt>
2023-01-24 16:34:25 -0600

    Stage 2/5: The user is the gamemaster

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,7 +1,10 @@
 package tictactoe
 
 fun main() {
-    println("X O X")
-    println("O X O")
-    println("X X O")
+    val str = readln()
+    println("---------")
+    println("| ${str[0]} ${str[1]} ${str[2]} |")
+    println("| ${str[3]} ${str[4]} ${str[5]} |")
+    println("| ${str[6]} ${str[7]} ${str[8]} |")
+    println("---------")
 }
